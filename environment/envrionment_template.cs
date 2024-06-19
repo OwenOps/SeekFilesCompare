d69fcda1dd7f58c916f77a97d130c5fa81b1413e
Owen <rebellerowen@gmail.com>
2024-06-19 23:19:11 +0200

    add environment example

---
+++ environment/envrionment_template.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SeekFilesCompare.environment
+{
+    internal class SettingsExample
+    {
+        public static string PathResult { get; } = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "results.xlsx");
+        public static string Server = "smtp.server"; // Example
+        public static int Port { get; } = 1234; // Example
+        public static string UserName { get; } = "your_mail";
+        public static string AppPassword = "your_app_password_mail";
+        public static string Name { get; } = "your_name";
+    }
+}
